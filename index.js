c9ffa45d560debf7fdc8dfbc5f50df998cd507b5
programadorisgod <catapiascastro@gmail.com>
2024-05-22 21:29:19 -0500

    major

---
+++ index.js
@@ -0,0 +1,21 @@
+export default function veaPrimo() {
+    const frases = [
+        '\n-> Cariculeticos, ya voy a dormir',
+        '-> a la final...',
+        '-> suerte contigo',
+        '-> alberto sobrino y el yorbi',
+        '-> ese manuel no hace es na',
+        '-> klk mamaguevo',
+        '-> ey loco que pasa valemia',
+        '-> hoy me voy con las maricas de farra',
+        '-> viva colombia viva falcaooo',
+        '-> pegelo socio pegueloooooo\n'
+    ];
+
+    for (const frase of frases) {
+      console.log(frase)
+    }
+
+}
+
+veaPrimo()
